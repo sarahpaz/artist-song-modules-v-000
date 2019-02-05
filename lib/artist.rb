dbb3e252b36ddeedc8e03371472f13aa2ad2d8d0
Sarah Paz <sarah.paz@gmail.com>
2019-02-05 03:08:16 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,14 +4,19 @@ class Artist
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
   include Paramable::InstanceMethods
+  include Memorable::InstanceMethods
 
   attr_accessor :name
   attr_reader :songs
 
   @@artists = []
 
+  # def initialize
+  #   self.class.all << self
+  #   @songs = []
+  # end
   def initialize
-    self.class.all << self
+    super
     @songs = []
   end
 
